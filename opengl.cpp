80c53f3e24f36d7e28b90a2cf3251823dd82f56f
songchung <syj2466@naver.com>
2019-09-25 13:27:15 +0900

    초기 커밋.

---
+++ opengl.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <glew.h>
+#include <freeglut.h>
+#include <ext.hpp>
+#include <gtc/matrix_transform.hpp>
+
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+
+void main(int argc, char** argv) // 윈도우 출력하고 콜백함수 설정 
+{ //--- 윈도우 생성하기
+	glutInit(&argc, argv); // glut 초기화
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA); // 디스플레이 모드 설정
+	glutInitWindowPosition(0, 0); // 윈도우의 위치 지정
+	glutInitWindowSize(800, 600); // 윈도우의 크기 지정
+	glutCreateWindow("Example1"); // 윈도우 생성(윈도우 이름)
+
+		//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+
+		exit(EXIT_FAILURE);
+	}
+	else
+		std::cout << "GLEW Initialized\n";
+
+	glutDisplayFunc(drawScene); // 출력 함수의 지정
+	glutReshapeFunc(Reshape); // 다시 그리기 함수 지정
+	glutMainLoop(); // 이벤트 처리 시작 
+}
+GLvoid drawScene() // 콜백 함수: 출력
+{
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f); // 바탕색을 ‘blue’로 지정
+	glClear(GL_COLOR_BUFFER_BIT); // 설정된 색으로 전체를 칠하기
+	// 그리기 부분 구현
+	//--- 그리기 관련 부분이 여기에 포함된다
+
+	glFlush(); // 화면에 출력하기
+}
+GLvoid Reshape(int w, int h) {
+	glViewport(0, 0, w, h);
+}
