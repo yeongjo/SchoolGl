2ff12e9ade2c6ec15986ca98bf9a375e2bd37d1b
songch <songchung2466@gmail.com>
2019-09-27 18:47:43 +0900

    0102 까지 함

--- 01_02/opengl.cpp
+++ 01_02/opengl.cpp
@@ -7,6 +7,7 @@
 GLvoid drawScene(GLvoid);
 GLvoid Reshape(int w, int h);
 void Keyboard(unsigned char key, int x, int y);
+void Mouse(int button, int state, int x, int y);
 
 void main(int argc, char** argv) // 윈도우 출력하고 콜백함수 설정 
 { //--- 윈도우 생성하기
@@ -30,22 +31,30 @@ void main(int argc, char** argv) // 윈도우 출력하고 콜백함수 설정
 	glutReshapeFunc(Reshape); // 다시 그리기 함수 지정
 
 	glutKeyboardFunc(Keyboard);
+	glutMouseFunc(Mouse);
 
 	glutMainLoop(); // 이벤트 처리 시작 
 }
 
 GLclampf r=1, g=1, b=1;
+GLclampf x = 0, y = 0, z = 1;
+
+GLfloat x1 = -0.5f, g_y1 = -0.5f, x2 = 0.5f, y2 = 0.5f;
 
 GLvoid drawScene() // 콜백 함수: 출력
 {
+	printf("drawScene\n");
 	glClearColor(r, g, b, 1.0f); // 바탕색을 ‘blue’로 지정
 	glClear(GL_COLOR_BUFFER_BIT); // 설정된 색으로 전체를 칠하기
 	
+	glColor3f(x, y, z);
 
+	glRectf(x1, g_y1, x2, y2);
 
 	glutSwapBuffers(); // 화면에 출력하기
 }
 GLvoid Reshape(int w, int h) {
+	printf("Reshape\n");
 	glViewport(0, 0, w, h);
 }
 bool isTimerEnd = false;
@@ -63,6 +72,8 @@ void timerFunc(int v) {
 
 void Keyboard(unsigned char key, int x, int y)
 {
+	printf("Keyboard\n");
+	return;
 	switch (key) {
 	case 'r': r=1,g=0,b=0; break;
 	case 'g':r = 0, g = 1, b = 0; break;
@@ -75,4 +86,22 @@ void Keyboard(unsigned char key, int x, int y)
 	case 'q': glutLeaveMainLoop(); break;
 	}
 	glutPostRedisplay();
+}
+
+void Mouse(int button, int state, int _x, int _y) {
+	printf("Mouse\n");
+	printf("%d\n", state);
+	float t_x = _x / (800.0f/2) - 1;
+	float t_y = -_y / (600.0f/2) + 1;
+	printf("%d %d %02f %02f\n", _x, _y, t_x, t_y);
+	if((t_x < x1 || x2 < t_x) || (t_y > y2 || t_y < g_y1))
+		r = f(), g = f(), b = f();
+	else
+		x = f(), y = f(), z = f();
+	glutPostRedisplay();
+	switch (state) {
+	case 0:
+
+		break;
+	}
 }
